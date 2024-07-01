e077392be085590c9cb4f7ced970f947eb78c461
Kaniuka <kaniukaroma@gmail.com>
2024-07-01 11:35:18 +0300

    Has been created the basic structure of the project.

---
+++ DorZnakUA.Api/Controller/ProjectController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DorZnakUA.Api.Controller;
+
+public class ProjectController : ControllerBase
+{
+    // GET
+    public IActionResult Index()
+    {
+    }
+}
